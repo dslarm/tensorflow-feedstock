c87698cca896594582c1224f350dee499b5245c6
David Lecomber <david.lecomber@arm.com>
2025-03-24 11:02:49 +0000

    - bump h5py up to 3.12.0 - first build available as binary on aarch64 - apply new step of regenerating requirements_lock file to take in our changes to requirements.in

--- recipe/build.sh
+++ recipe/build.sh
@@ -221,6 +221,8 @@ build --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include
 build --cpu=${TARGET_CPU}
 build --local_cpu_resources=${CPU_COUNT}
 EOF
+# update the requirements list - we've patched it
+bazel run //ci/official/requirements_updater:requirements.update --repo_env=HERMETIC_PYTHON_VERSION=$PY_VER
 
 # Update TF lite schema with latest flatbuffers version
 pushd tensorflow/compiler/mlir/lite/schema

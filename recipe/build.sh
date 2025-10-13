f7bf7cd9c4e5295a7d3929e6904ccf6ca405f4a6
Isuru Fernando <ifernando@openteams.com>
2025-10-13 20:58:14 +0000

    Add a protobuf hack

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+pushd ${PREFIX}/include/python/google/protobuf
+  patch -o message_lite.h.new -p0 < ${SRC_DIR}/protobuf_hack.diff
+  rm message_lite.h
+  mv message_lite.h.new message_lite.h
+popd
+
 for ver in 3.9 3.10 3.11 3.12; do
   export PY_VER=$ver
   echo "Building for $PY_VER"
